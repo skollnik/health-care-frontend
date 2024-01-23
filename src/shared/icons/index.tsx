cb0a492194c7c5de930169a68f4c6e015a4b4c9b
Dobrica <dobrica.skolnik00@gmail.com>
2024-01-23 11:02:52 +0100

    error page

--- src/shared/icons/index.tsx
+++ src/shared/icons/index.tsx
@@ -1,4 +1,5 @@
 import { HealthCareIcon } from "./health-care.icon";
 import { RightArrowIcon } from "./right-arrow.icon";
+import { DoctorErrorIcon } from "./doctor-error.icon";
 
-export { HealthCareIcon, RightArrowIcon };
+export { HealthCareIcon, RightArrowIcon, DoctorErrorIcon };
--- src/shared/pages/error.page.tsx
+++ src/shared/pages/error.page.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import { DoctorErrorIcon } from "../icons";
 
 export const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center h-screen bg-[#f6f4d2]">
+      <h1 className="font-bold text-9xl">404</h1>
+      <div className="flex items-center justify-center">
+        <DoctorErrorIcon />
+        <p className="text-xl">The page you were looking for does not exist</p>
+      </div>
+    </div>
+  );
+};
